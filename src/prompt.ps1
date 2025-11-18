80769fa9ca3e789832a0f4f1bca6d19606b4a617
Serhii Pimenov <serhii@pimenov.com.ua>
2025-11-18 19:41:05 +0200

    Add custom PowerShell prompt function

---
+++ src/prompt.ps1
@@ -0,0 +1,36 @@
+$Env:SHOW_PROMPT_TIME = "NO"
+
+function prompt {
+    $uptime = Get-Uptime -ErrorAction SilentlyContinue
+    $username = $env:USERNAME + ":"
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+    $folder = Split-Path -Path (Get-Location) -Leaf
+    $nodeVersion = (node -v).Trim()
+    $packageJson = Test-Path package.json -PathType Leaf
+    $currentTime = $(Get-Date -Format "dddd dd-MM-yyyy HH:mm")
+
+
+    if ($Env:SHOW_PROMPT_TIME -eq "YES") {
+        if ($uptime) {
+            Write-Host "💻 Uptime: " -NoNewLine -ForegroundColor Gray
+            Write-Host "$uptime, " -NoNewLine -ForegroundColor Magenta
+        } else {
+            Write-Host "💻 Uptime: Unknown " -NoNewLine -ForegroundColor Gray
+        }
+        Write-Host "⌚ $currentTime " -ForegroundColor Yellow
+    }
+    Write-Host "🫀 $username "  -NoNewLine -ForegroundColor Cyan
+    Write-Host "📂 $folder " -NoNewLine -ForegroundColor Green
+
+    if ($currentBranch) {
+        Write-Host "🌵 $currentBranch " -NoNewLine -ForegroundColor White
+    }
+
+    if ($packageJson) {
+        Write-Host "[👽 $nodeVersion] " -NoNewLine -ForegroundColor Green
+    }
+
+    Write-Host ""
+    Write-Host "└─❯" -NoNewLine -ForegroundColor Yellow
+    return " "
+}
