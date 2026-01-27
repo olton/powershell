3497471ed3cec85314488fb260f1f40c9bfec062
Serhii Pimenov <serhii@pimenov.com.ua>
2026-01-27 16:49:19 +0200

    Update imports.ps1

--- src/imports.ps1
+++ src/imports.ps1
@@ -1,1 +1,6 @@
+# Перевірка та інсталяція модуля Terminal-Icons
+if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
+    Write-Host "Installing Terminal-Icons module..." -ForegroundColor Yellow
+    Install-Module -Name Terminal-Icons -Force -Scope CurrentUser
+}
 Import-Module -Name Terminal-Icons
