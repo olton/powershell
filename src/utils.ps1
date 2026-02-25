fa381d2caea077f68d2e180567a624fd3a5ed198
Serhii Pimenov <serhii@pimenov.com.ua>
2026-02-25 19:12:40 +0200

    Add utility functions for formatting sizes and extracting VSCode extensions

--- src/utils.ps1
+++ src/utils.ps1
@@ -1,3 +1,48 @@
+# Usefulness functions
+function Format-Size {
+    param([long]$Bytes)
+    
+    if ($Bytes -ge 1TB) {
+        return "{0:F1} TB" -f ($Bytes / 1TB)
+    } elseif ($Bytes -ge 1GB) {
+        return "{0:F1} GB" -f ($Bytes / 1GB)
+    } elseif ($Bytes -ge 1MB) {
+        return "{0:F1} MB" -f ($Bytes / 1MB)
+    } elseif ($Bytes -ge 1KB) {
+        return "{0:F1} KB" -f ($Bytes / 1KB)
+    } else {
+        return "{0} B" -f $Bytes
+    }
+}
+
+function extract-vscode-extensions {
+    param (
+        [string]$File = "vscode-extensions",
+        [switch]$Install,
+        [switch]$Linux
+    )
+
+    $ext = if ($Install) { 
+        if ($Linux) { 
+            ".sh" 
+            } 
+        else {
+            ".ps1" 
+        }
+    } else { ".txt" }
+    $outputFile = "$File$ext"
+    
+    Write-Host " "
+    Write-Host "Extracting installed VSCode extensions to '$outputFile'..." -NoNewLine
+    if ($Install) {
+        code --list-extensions | % { "code --install-extension $_" } | Set-Content $outputFile -Encoding UTF8
+    } else {
+        code --list-extensions | Out-File -FilePath $outputFile -Encoding UTF8
+    }
+    Write-Host "Done." -ForegroundColor Green
+    Write-Host " "
+}
+
 function errors {
     param (
         [int]$Count = 5
